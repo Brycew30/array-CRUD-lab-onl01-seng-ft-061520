c35d75a44ca9a9e027d4ee9aca7671f9996b776c
Bryce Whittington <brycew30@gmail.com>
2020-06-16 18:57:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+ random_array = ["red", "orange", "yellow", "green"]
+ random_array << "arrays!"
 end
 
 def remove_element_from_end_of_array(array)
